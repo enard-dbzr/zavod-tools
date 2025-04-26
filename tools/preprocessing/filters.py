1796b026726de20298a0ef79eada820f744f5aa8
enard <serg.miass340@gmail.com>
2025-04-26 13:16:27 +0500

    add file filter by all columns

--- tools/preprocessing/filters.py
+++ tools/preprocessing/filters.py
@@ -6,7 +6,7 @@ import pandas as pd
 from tools.preprocessing.transforms import Transform
 
 
-class FilterOutliers(Transform):
+class RangeFileFilter(Transform):
     def __init__(self, file_paths=None, limits=None):
         if limits is None:
             limits = {}
@@ -25,7 +25,7 @@ class FilterOutliers(Transform):
             for idx, (col, begin, end, method, *_) in outliers_df.iterrows():
                 if pd.notnull(col):
                     if method == "nan" or pd.isna(method):
-                        df.loc[begin:end, col] = np.nan
+                        df.loc[begin:end, col if col != "*" else slice(None)] = np.nan
 
         # comp = df.copy()
         for col, (lb, ub) in self.limits.items():
