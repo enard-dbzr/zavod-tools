2d57e2d968ebc3350e4cda1ed3ea6fbca71e0d11
enard <serg.miass340@gmail.com>
2025-04-26 13:27:50 +0500

    add filter pipeline preset

--- tools/consts.py
+++ tools/consts.py
@@ -38,3 +38,29 @@ limits = {
 
 ws = torch.tensor([1.63778039, 1.20387111, 1.20317918, 1.32377653, 22.05927316,
                    23.90044874, 97.1838969, 1.09202928])
+
+
+def get_filter_pipeline():
+    from tools.preprocessing.filters import IQRFilter, ColumnValueFilter, RangeFileFilter
+    from tools.preprocessing.transforms import TransformPipeline, KMeansClusterTransform, SpecifiedColumnsTransform
+
+    iqr_filter = IQRFilter(column_specified_settings={
+        TARGETS[0]: (5, 0.5, "10min"),
+        TARGETS[1]: (7, 0.9, "10min"),
+        TARGETS[2]: (1.5, 0.5, "20min"),
+        TARGETS[3]: (5, 0.5, "5min"),
+        TARGETS[4]: (5, 0.5, "20min"),
+        TARGETS[5]: (1.5, 0.5, "20min"),
+        TARGETS[6]: (3, 0.5, "20min"),
+        TARGETS[7]: (5, 0.5, "10min"),
+    })
+
+    filter_pipeline = TransformPipeline(
+        KMeansClusterTransform(n_clusters=2, random_state=2025),
+        ColumnValueFilter('cluster', include_indices=["2019-01-08", "2019-07", "2019-12"], mode="include",
+                          use_latest=True, gap="24h", shift="8h"),
+        SpecifiedColumnsTransform(iqr_filter, TARGETS),
+        RangeFileFilter(["PEMS_PD/nans_segments.ods"])
+    )
+
+    return filter_pipeline
