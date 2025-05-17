4db35390cd612a18c8a3b4572c83dd737d8ec0b8
enard <serg.miass340@gmail.com>
2025-05-17 11:55:36 +0500

    exterminate NormalizedCovarianceLoss

--- tools/objectives/losses.py
+++ tools/objectives/losses.py
@@ -5,34 +5,6 @@ import torch
 from tools.objectives.metrics import PredictionBasedMetric, Metric
 
 
-class NormalizedCovarianceLoss(PredictionBasedMetric):
-    def __init__(self, corr_matr: torch.Tensor, std: torch.Tensor):
-        super().__init__(None)
-
-        self.corr_matr = corr_matr.unsqueeze(0)
-
-        self.outer_std = torch.outer(std, std).unsqueeze(0)
-
-    def compute(self, y_pred: torch.Tensor, y_true: torch.Tensor, x: torch.Tensor, iloc) -> torch.Tensor:
-        xy_data = torch.concat([x, y_pred], dim=-1)
-
-        means = xy_data.nanmean(dim=1, keepdim=True)
-
-        # # Заменяем NaN на среднее по колонке (вдоль window).
-        # # От этого не изменится ковариация, за исключением нормировки на количество
-        # xy_data = torch.where(xy_data.isnan(), means, xy_data)
-
-        centered = xy_data - means
-
-        corr = torch.bmm(centered.transpose(1, 2), centered) / self.outer_std
-
-        delta_corr = (corr - self.corr_matr)
-
-        corr_loss = torch.linalg.matrix_norm(delta_corr, dim=(1, 2)).nanmean()
-
-        return corr_loss
-
-
 class NormalizedCovarianceWindowLoss(PredictionBasedMetric):
     def __init__(self, cov_adapter: Callable[[torch.Tensor], torch.Tensor],
                  vanish_xx: bool = True,
