594ab37185a815f198f7f23de92e49c56727e71c
enard <serg.miass340@gmail.com>
2025-05-20 17:07:35 +0500

    add warning for end freq

--- tools/dataset_adapters.py
+++ tools/dataset_adapters.py
@@ -1,3 +1,5 @@
+import warnings
+
 import pandas as pd
 import torch
 
@@ -5,12 +7,15 @@ from tools.dataset import PEMSDataset
 
 
 class CovarianceDatasetAdapter:
-    def __init__(self, dataset: PEMSDataset, resample="2ME", device="cpu"):
+    def __init__(self, dataset: PEMSDataset, resample="100YS", device="cpu"):
         self.dataset = dataset
+
+        if resample.endswith("E"):
+            warnings.warn("Be careful with end frequency in resample")
 
         df = pd.concat([self.dataset.x, self.dataset.y], axis=1)
 
-        group = df.groupby(pd.Grouper(freq=resample, closed='left'))
+        group = df.groupby(pd.Grouper(freq=resample, closed="left"))
 
         cov = group.cov().to_numpy(dtype='float32')
         cov = cov.reshape((-1, cov.shape[-1], cov.shape[-1]))
@@ -20,5 +25,6 @@ class CovarianceDatasetAdapter:
         self.cov = torch.tensor(cov, device=device)
         self.stat_loc = torch.tensor(stat_loc, device=device)
         print(f"Grouped into {len(group)} group(s).")
+
     def __call__(self, idx: torch.Tensor) -> torch.Tensor:
         return self.cov[self.stat_loc[idx]]
